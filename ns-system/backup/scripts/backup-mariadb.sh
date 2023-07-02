7d0ba9d53139dd533c68d4537f0b87242c91b111
motoki317 <motoki317@gmail.com>
2023-07-02 18:28:57 +0900

    install mysqldump online

--- ns-system/backup/scripts/backup-mariadb.sh
+++ ns-system/backup/scripts/backup-mariadb.sh
@@ -2,6 +2,9 @@
 
 set -euxo pipefail
 
+apk update
+apk add mysql-client
+
 set +x
 mysqldump -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" --all-databases --single-transaction --protocol=TCP --default-character-set=utf8mb4 | gzip > /root/showcase-mariadb-backup.gz
 set -x
