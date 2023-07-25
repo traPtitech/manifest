2c909c3f4dcece60e7e7af60618349c07e465868
motoki317 <motoki317@gmail.com>
2023-07-25 22:44:19 +0900

    add traq preview (#47)
    * add traq preview
    
    * format

--- check.sh
+++ check.sh
@@ -9,7 +9,7 @@ contains () {
   return 1
 }
 
-skip_dirs=("auth-template" "preview-ns-template" "promtail")
+skip_dirs=("auth-template" "preview-ns-template" "preview-traq-template" "promtail")
 for directory in $(echo ./*/ | tr -d './' | tr -d '/'); do
   if ! contains "$directory" "${skip_dirs[@]}"; then
     kubectl create namespace "$directory" --dry-run=client -o yaml | kubectl apply -f -
