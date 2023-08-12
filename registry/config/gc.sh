1749886b49843433efa64c5b307b9faffc2ac7ac
motoki317 <motoki317@gmail.com>
2023-08-12 11:43:45 +0900

    run gc every 1d

--- registry/config/gc.sh
+++ registry/config/gc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-  sleep 3600
+  sleep 86400
   echo "[gc.sh] Running garbage collection..."
   registry garbage-collect "$1" --delete-untagged
   echo "[gc.sh] Garbage collection complete."
