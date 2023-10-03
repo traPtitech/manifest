9ce933c85babce087404e6399a66a0406a7803b8
motoki317 <motoki317@gmail.com>
2023-10-04 07:50:36 +0900

    Run registry gc by argo workflow cron

--- registry/config/gc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while true; do
-  sleep 86400
-  echo "[gc.sh] Running garbage collection..."
-  registry garbage-collect "$1" --delete-untagged
-  echo "[gc.sh] Garbage collection complete."
-done
