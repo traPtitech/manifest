b74d579369662e7755c504ee3f53dc94a893ca8e
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-04-19 01:11:47 +0900

    🐛 リポジトリ設定を修正

--- .github/renovate-bot.js
+++ .github/renovate-bot.js
@@ -4,22 +4,8 @@ module.exports = {
   "repositories": ["traPtitech/manifest"],
   "hostRules": [
     {
-      "matchHost": "https://api.github.com/repos/traPtitech/portal",
-      "username": "trapyojo",
-      "token": process.env.RENOVATE_GITHUB_PAT
-    },
-    {
-      "matchHost": "https://github.domain.com/api/v3/repos/traPtitech/portal",
-      "username": "trapyojo",
-      "token": process.env.RENOVATE_GITHUB_PAT
-    },
-    {
-      "matchHost": "https://api.github.com/repos/traPtitech/portal-UI",
-      "username": "trapyojo",
-      "token": process.env.RENOVATE_GITHUB_PAT
-    },
-    {
-      "matchHost": "https://github.domain.com/api/v3/repos/traPtitech/portal-UI",
+      "hostType": "docker",
+      "matchHost": "ghcr.io",
       "username": "trapyojo",
       "token": process.env.RENOVATE_GITHUB_PAT
     }
