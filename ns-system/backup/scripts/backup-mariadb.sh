663f3ec031abb8fc2d0166906a6e80ec8c9314a6
pirosiki <130675136+pirosiki197@users.noreply.github.com>
2025-05-17 19:00:42 +0900

    Revert "output backup log (#885)" (#890)
    This reverts commit 738d66617d4cad834f1ae25178e1b6a5102e5785.

--- ns-system/backup/scripts/backup-mariadb.sh
+++ ns-system/backup/scripts/backup-mariadb.sh
@@ -9,5 +9,5 @@ set +x
 mysqldump -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" --all-databases --single-transaction --protocol=TCP --default-character-set=utf8mb4 | gzip > /root/showcase-mariadb-backup.gz
 set -x
 
-gcloud auth --log-http activate-service-account backup@trap-sysad.iam.gserviceaccount.com --key-file=/keys/key.json
-gsutil cp -L /root/backup-cp.log /root/showcase-mariadb-backup.gz gs://trap-services-backup/
+gcloud auth activate-service-account backup@trap-sysad.iam.gserviceaccount.com --key-file=/keys/key.json
+gsutil cp /root/showcase-mariadb-backup.gz gs://trap-services-backup/
