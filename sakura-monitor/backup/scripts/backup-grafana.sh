23b629249e7bef551d6e7480c6d2ca1425492b74
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-06-18 01:11:28 +0900

    `monitor`をoverlays化

--- sakura-monitor/backup/scripts/backup-grafana.sh
+++ sakura-monitor/backup/scripts/backup-grafana.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 gzip < /data/grafana.db > /data/grafana.db.gz
 
 gcloud auth activate-service-account backup@trap-sysad.iam.gserviceaccount.com --key-file=/keys/key.json
-gsutil cp /data/grafana.db.gz gs://trap-services-backup/
+gsutil cp /data/grafanad-sakura.db.gz gs://trap-services-backup/
