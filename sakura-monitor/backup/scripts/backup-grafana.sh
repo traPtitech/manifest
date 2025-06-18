6b19f6d1ed96599e5e92ae486c3cdef3032cb091
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-06-18 19:04:44 +0900

    バックアップのファイル名を修正

--- sakura-monitor/backup/scripts/backup-grafana.sh
+++ sakura-monitor/backup/scripts/backup-grafana.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-gzip < /data/grafana.db > /data/grafana.db.gz
+gzip < /data/grafana.db > /data/grafana-sakura.db.gz
 
 gcloud auth activate-service-account backup@trap-sysad.iam.gserviceaccount.com --key-file=/keys/key.json
 gsutil cp /data/grafanad-sakura.db.gz gs://trap-services-backup/
