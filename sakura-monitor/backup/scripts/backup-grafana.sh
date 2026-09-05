cef3d471f97b97711b37783ed3946cdc704356bf
けんたろう <71170923+Kentaro1043@users.noreply.github.com>
2026-09-05 20:02:42 +0900

    fix(sakura-monitor): 🐛 GrafanaバックアップをMySQL対応 (#1823)

--- sakura-monitor/backup/scripts/backup-grafana.sh
+++ sakura-monitor/backup/scripts/backup-grafana.sh
@@ -2,7 +2,18 @@
 
 set -euxo pipefail
 
-gzip < /data/grafana.db > /data/grafana-sakura.db.gz
+apk add --no-cache mariadb-client
+
+set +x
+mariadb-dump \
+  --host="$DB_HOST" \
+  --port="$DB_PORT" \
+  --user="$DB_USER" \
+  --password="$DB_PASSWORD" \
+  --single-transaction \
+  --default-character-set=utf8mb4 \
+  "$DB_NAME" | gzip > /tmp/grafana-sakura.db.gz
+set -x
 
 gcloud auth activate-service-account backup@trap-sysad.iam.gserviceaccount.com --key-file=/keys/key.json
-gsutil cp /data/grafana-sakura.db.gz gs://trap-services-backup/
+gsutil cp /tmp/grafana-sakura.db.gz gs://trap-services-backup/
